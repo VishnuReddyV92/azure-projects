17e9c01c36006dc113b4f2e1a482199aa6a26367
VishnuReddyV92 <vishnureddyv92@gmail.com>
2026-09-23 12:46:15 +0530

    Trigger TFC VCS run after fixing path pattern

--- projects/project-2-acr-appservice/terraform/app_service.tf
+++ projects/project-2-acr-appservice/terraform/app_service.tf
@@ -21,6 +21,7 @@ resource "azurerm_linux_web_app" "app" {
       docker_registry_url = "https://mcr.microsoft.com"
     }
     health_check_path = "/health"
+    health_check_eviction_time_in_min = 2
   }
 
   app_settings = {
@@ -48,6 +49,7 @@ resource "azurerm_linux_web_app_slot" "staging" {
       docker_registry_url = "https://mcr.microsoft.com"
     }
     health_check_path = "/health"
+    health_check_eviction_time_in_min = 2
   }
 
   lifecycle {
