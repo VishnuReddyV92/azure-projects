c22ee5c86cd4b7b67f5f70eb76049765313c3704
VishnuReddyV92 <vishnureddyv92@gmail.com>
2026-09-23 14:15:55 +0530

    update src

--- projects/project-2-acr-appservice/app/src/app.py
+++ projects/project-2-acr-appservice/app/src/app.py
@@ -12,4 +12,4 @@ def health():
     return jsonify(status="healthy"), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)# trigger build 1790153103
