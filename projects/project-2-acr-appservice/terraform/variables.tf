9b128740aca79b664f3f37b1cc09c00b32fc0a03
VishnuReddyV92 <vishnureddyv92@gmail.com>
2026-09-23 13:06:37 +0530

    fix acr error and ASP quota issue

--- projects/project-2-acr-appservice/terraform/variables.tf
+++ projects/project-2-acr-appservice/terraform/variables.tf
@@ -2,7 +2,7 @@ variable "project_name"         { default = "myapp" }
 variable "location"             { default = "centralindia" }
 variable "environment"          { default = "prod" }
 variable "acr_sku"              { default = "Basic" }
-variable "app_service_plan_sku" { default = "B1" }
+variable "app_service_plan_sku" { default = "S1" }
 variable "unique_suffix" {
   default = "vvr01"   # change to something unique to you, keep it short, lowercase, alphanumeric only
 }
